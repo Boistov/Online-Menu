3b08b9abf07ebae9b52773c32987e1092935bd9e
Boistov <sboistov@gmail.com>
2024-07-21 23:37:10 -0700

    hello

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Review, Order, Dish, Category
 
 
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(Dish)
+admin.site.register(Category)
